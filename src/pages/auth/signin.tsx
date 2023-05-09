26ec6a5dc3d3cca1cd1bec647446536e0bbf36b8
Loïca <95331041+NiceGuyMe@users.noreply.github.com>
2023-05-09 23:05:51 +0300

    feat: use react-hook-form for login validation

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const signin = () => {
+const Signin = () => {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
     <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -45,4 +45,4 @@ const signin = () => {
   )
 }
 
-export default signin
+export default Signin
