630106ee7f39acd91f50d228b43d1ece2eebcd78
Loïca <95331041+NiceGuyMe@users.noreply.github.com>
2023-05-21 22:41:27 +0300

    feat: protect pages if !TOKEN

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -8,9 +8,9 @@ const SearchBar = () => {
           <svg
             fill="none"
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             viewBox="0 0 24 24"
             className="w-6 h-6 text-gray-300"
           >
